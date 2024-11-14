a7d780c27dda4919845fcb960463a6c506206916
ngoguened <tillouturnip@gmail.com>
2024-11-13 22:22:22 -0500

    fixed proto-resources config import

--- server/poke_resources.py
+++ server/poke_resources.py
@@ -1,12 +1,16 @@
 from google.protobuf.text_format import Parse
 import grpc
-# import proto.resources_pb2
+import proto.resources_pb2 as resources
 import proto.config_pb2
 import server.model as model
 
 def read_moves_and_templates():
     moves = dict()
     templates = dict()
+
+    # print(resources.GetMovesRequest())
+    # print(resources.GetTemplatesRequest())
+    
     with open("server/moves_and_templates.textproto", encoding="ascii") as moves_and_templates_file:
         moves_and_templates_text_lines = [line for line in moves_and_templates_file.readlines()]
         moves_and_templates_text = ' '.join(moves_and_templates_text_lines)
